95593c67fb3d9d9938cb0b4e770864e7f7cc16f6
Jack Blakeston <jackblakeston93@gmail.com>
2022-04-06 23:35:01 +0200

    Pads can now be set as active or inactive

--- client/src/components/Pad.js
+++ client/src/components/Pad.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Pad (props) {
-  return <div className='pad'></div>;
+  return (
+    <div
+      className={'pad ' + (props.state ? 'active' : '')}
+      onClick={() => props.toggleActive(props.rowIndex, props.id)}>
+    </div>
+  );
 }
 
 export default Pad;
--- client/src/components/Pads.js
+++ client/src/components/Pads.js
@@ -17,13 +17,29 @@ function Pads () {
 
   const [pads, setPads] = useState(initialPads)
 
+  function toggleActive (rowIndex, id) {
+    let padsCopy = [...pads];
+    let padState = padsCopy[rowIndex][id];
+    if (padState === 1) {
+      pads[rowIndex][id] = 0;
+    } else {
+      pads[rowIndex][id] = 1;
+    }
+    setPads(padsCopy);
+  }
+
   return (
     <div className='pads'>
       {pads.map((row, rowIndex) => {
         return (
           <div className='row' key={rowIndex}>a
             {row.map((pad, index) => {
-              return <Pad key={index} />
+              return <Pad
+                key={index}
+                rowIndex={rowIndex}
+                id={index}
+                state={pad}
+                toggleActive={toggleActive} />
             })}
           </div>
         )
