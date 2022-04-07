5e982dc3623fb1c873fe0577d689b67a41e81d3e
Jack Blakeston <jackblakeston93@gmail.com>
2022-04-07 08:50:37 +0200

    Implement useInterval instead of setInterval

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import Pads from './components/Pads';
 import Controls from './components/Controls';
@@ -9,37 +9,63 @@ function App () {
   const [playing, setPlaying] = useState(false);
   const [pos, setPos] = useState(0);
   const [bpm, setBpm] = useState(220);
-  const [timerId, setTimerId] = useState();
+  // const [timerId, setTimerId] = useState();
 
-  useEffect(() => {
+  function useInterval(callback, delay) {
+    const intervalRef = React.useRef();
+    const callbackRef = React.useRef(callback);
 
-  }, [pos]);
+    // Remember the latest callback:
+    //
+    // Without this, if you change the callback, when setInterval ticks again, it
+    // will still call your old callback.
+    //
+    // If you add `callback` to useEffect's deps, it will work fine but the
+    // interval will be reset.
+
+    React.useEffect(() => {
+      callbackRef.current = callback;
+    }, [callback]);
+
+    // Set up the interval:
+
+    React.useEffect(() => {
+      if (typeof delay === 'number') {
+        intervalRef.current = window.setInterval(() => callbackRef.current(), delay);
+
+        // Clear interval if the components is unmounted or the delay changes:
+        return () => window.clearInterval(intervalRef.current);
+      }
+    }, [delay]);
+
+    // Returns a ref to the interval ID in case you want to clear it manually:
+    return intervalRef;
+  }
+  ////////
 
 
   function togglePlaying () {
-    if (playing) clearInterval(timerId);
-    else setTimer();
+    if (playing) clearInterval(newTimerId);
 
     setPlaying(!playing);
   }
 
-  function setTimer () {
-    const newTimerId = setInterval(() => tick(), calculateTempo(bpm));
-    setTimerId(newTimerId);
-  }
+  // function setTimer () {
+  //   const newTimerId = setInterval(() => tick(), calculateTempo(bpm));
+  //   setTimerId(newTimerId);
+  // }
+
+  const newTimerId = useInterval(tick, playing ? calculateTempo(bpm) : null)
 
   function tick () {
     let currentPos = pos;
     currentPos++;
 
     if (currentPos > 7) currentPos = 0;
-    setPos(pos);
+    setPos(currentPos);
     console.log('CURRENT POSITION', pos);
   }
-  // TODO setinterval not getting updated state values.
-  // TODO how can we fix this? callback in setpos does not work
-  // TODO i saw that there are libraries with useInterval functions
-  // TODO but i would like to solve it without doing that
+  // ?? Any way to do this without useInterval hook?
 
   function calculateTempo (bpm) {
     return 60000 / bpm;
