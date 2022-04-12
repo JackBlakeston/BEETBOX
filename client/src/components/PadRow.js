cd53a20e7eb49f8e5ab9ca4f9eab584c86d97db5
Jack Blakeston <jackblakeston93@gmail.com>
2022-04-12 12:21:41 +0200

    Add style and delete audio files

--- client/src/components/PadRow.js
+++ client/src/components/PadRow.js
@@ -1,19 +1,7 @@
 import { useEffect } from 'react';
 import useSound from 'use-sound';
 
-import Pad from './Pad'
-
-// Audio file imports
-// import kick1 from './assets/sounds/kick_1.wav';
-// import { useEffect, useState } from 'react';
-// import { Test } from '../audio-service';
-// import kick2 from './assets/sounds/kick_2.wav';
-// import snare from './assets/sounds/snare.wav';
-// import hhClosed from './assets/sounds/hh_closed.wav';
-// import hhOpen from './assets/sounds/hh_open.wav';
-// import shaker from './assets/sounds/shaker.wav';
-// import clap from './assets/sounds/clap.wav';
-// import scratch from './assets/sounds/scratch.wav';
+import Pad from './Pad';
 
 function PadRow ({soundFileUrl, pads, pos, toggleActive, isPlaying, rowIndex, sampleName}) {
 
@@ -26,31 +14,21 @@ function PadRow ({soundFileUrl, pads, pos, toggleActive, isPlaying, rowIndex, sa
     }
   }, [pos, isPlaying, playSound]);
 
-
-  // const [playKick2] = useSound(kick2);
-  // const [playSnare] = useSound(snare);
-  // const [playHhClosed] = useSound(hhClosed);
-  // const [playHhOpen] = useSound(hhOpen);
-  // const [playShaker] = useSound(shaker);
-  // const [playClap] = useSound(clap);
-  // const [playScratch] = useSound(scratch);
-
   return (
-    <>
-        <p>{sampleName}</p>
-          <div className='row'>
-            {pads.map((pad, index) => {
-              return <Pad
-                key={index}
-                rowIndex={rowIndex} // Needs to be index of current row
-                id={index}
-                state={pad}
-                pos={pos}
-                toggleActive={() => toggleActive(rowIndex, index)} />
-            })}
-          </div>
-
-    </>
+    <div className='row-container'>
+      <p>{sampleName}</p>
+        <div className='row'>
+          {pads.map((pad, index) => {
+            return <Pad
+              key={index}
+              rowIndex={rowIndex}
+              id={index}
+              state={pad}
+              pos={pos}
+              toggleActive={() => toggleActive(rowIndex, index)} />
+          })}
+        </div>
+    </div>
 
   )
 }
